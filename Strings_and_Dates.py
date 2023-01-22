e3274f77fa4be65a468f68e7c10e1339fc37bb18
chaoticinsane <insaneruin69@gmail.com>
2023-01-22 13:54:13 -0800

    Lab 2 Strings and Dates

--- Strings_and_Dates.py
+++ Strings_and_Dates.py
@@ -1,1 +1,14 @@
-
+while True:
+    question="Enter Date: " 
+    user_input = input(question)
+    if user_input != "-1":
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        if ',' in user_input:
+            noComma = user_input.replace(',', '')
+            inputArr = noComma.split(' ')
+            month = months.index(inputArr[0])
+            print(f'{month +1 }/{inputArr[1]}/{inputArr[2]}')
+        else:
+            print('Date entered is not valid')
+    else:
+        break 
